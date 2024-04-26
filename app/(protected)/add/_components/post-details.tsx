f4f0035c326922e2590f6ac56bd35131977151cd
Rati <rziraqashvili@gmail.com>
2024-04-26 15:08:16 +0400

    created steps ui

--- app/(protected)/add/_components/post-details.tsx
+++ app/(protected)/add/_components/post-details.tsx
@@ -1,7 +1,47 @@
+import { Separator } from "@/components/ui/separator";
+
 export const PostDetails = () => {
-    return (
-        <div className="">
-            ddxxf
+  const steps = [
+    {
+      name: "ძირითადი მახასიათებლები",
+      isFinished: false,
+    },
+    {
+      name: "მდებარეობა და განბაჟება",
+      isFinished: false,
+    },
+    {
+      name: "ფოტო / ვიდეო",
+      isFinished: false,
+    },
+    {
+      name: "ფასი",
+      isFinished: false,
+    },
+    {
+      name: "კონტაქტი",
+      isFinished: false,
+    },
+  ];
+
+  return (
+    <div className="bg-white rounded-xl w-full lg:w-[24%] p-5 flex flex-col">
+      {steps.map((step) => (
+        <div className="flex gap-4">
+          <div className="flex flex-col items-center">
+            <div className="bg-white rounded-full border-2 border-gray-200 h-4 w-4 cursor-pointer"></div>
+            {step.name !== "კონტაქტი" && (
+              <Separator
+                className="h-10 w-[0.1rem] my-1"
+                orientation="vertical"
+              />
+            )}
+          </div>
+          <ul className="text-[0.75rem] text-gray-400 -mt-[0.165rem] cursor-pointer tracking-wide">
+            {step.name}
+          </ul>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
--- app/(protected)/add/page.tsx
+++ app/(protected)/add/page.tsx
@@ -3,7 +3,7 @@ import { PostForms } from "./_components/post-forms";
 
 const AddPage = async () => {
   return (
-    <div className="flex gap-2 md:flex-row flex-col relative w-[90%] mx-auto md:w-[77%] lg:w-[75%]">
+    <div className="flex gap-4 lg:flex-row flex-col relative w-[90%] mx-auto md:w-[77%] lg:w-[75%]">
       <PostDetails />
       <PostForms />
     </div>
