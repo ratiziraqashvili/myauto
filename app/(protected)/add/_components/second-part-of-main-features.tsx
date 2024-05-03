5e53b1e5f994c5afbb65a9ccdfd1f66b71e3d176
Rati <rziraqashvili@gmail.com>
2024-05-03 12:40:58 +0400

    created resusable component for toggle roup field

--- app/(protected)/add/_components/second-part-of-main-features.tsx
+++ app/(protected)/add/_components/second-part-of-main-features.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/form";
 import { SmallFormHeading } from "./small-form-heading";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { ToggleGroupField } from "./toggle-group-field";
 
 interface SecondPartOfMainFeaturesProps {
   control: Control<z.infer<typeof formSchema>>;
@@ -21,150 +22,43 @@ export const SecondPartOfMainFeatures = ({
 }: SecondPartOfMainFeaturesProps) => {
   return (
     <div className="flex flex-col gap-5 px-6 py-6">
-      <Controller
+      <ToggleGroupField<typeof formSchema>
         control={control}
         name="steeringWheel"
-        render={({ field }) => (
-          <FormItem className="">
-            <FormLabel>
-              <SmallFormHeading label="საჭე" />
-            </FormLabel>
-            <FormMessage>
-              {errors.steeringWheel?.message as React.ReactNode}
-            </FormMessage>
-            <FormControl>
-              <ToggleGroup
-                value={field.value}
-                onValueChange={(value) => field.onChange(value)}
-                className="space-x-4 pt-2"
-                type="single"
-              >
-                <ToggleGroupItem type="button" variant="outline" value="Right">
-                  მარჯვენა
-                </ToggleGroupItem>
-                <ToggleGroupItem type="button" variant="outline" value="Left">
-                  მარცხენა
-                </ToggleGroupItem>
-              </ToggleGroup>
-            </FormControl>
-          </FormItem>
-        )}
+        label="საჭე"
+        errors={errors}
+        options={[
+          { value: "Right", label: "მარჯვენა" },
+          { value: "Left", label: "მარცხენა" },
+        ]}
       />
-      <Controller
+      <ToggleGroupField
         control={control}
         name="transmission"
-        render={({ field }) => (
-          <FormItem className="">
-            <FormLabel>
-              <SmallFormHeading label="ტრანსმისია" />
-            </FormLabel>
-            <FormMessage>
-              {errors.transmission?.message as React.ReactNode}
-            </FormMessage>
-            <FormControl>
-              <ToggleGroup
-                value={field.value}
-                onValueChange={(value) => field.onChange(value)}
-                className="space-x-4 pt-2"
-                type="single"
-              >
-                <ToggleGroupItem type="button" variant="outline" value="Manual">
-                  მექანიკა
-                </ToggleGroupItem>
-                <ToggleGroupItem
-                  type="button"
-                  variant="outline"
-                  value="Automatic"
-                >
-                  ავტომატიკა
-                </ToggleGroupItem>
-                <ToggleGroupItem
-                  type="button"
-                  variant="outline"
-                  value="Tiptronic"
-                >
-                  ტიპტრონიკი
-                </ToggleGroupItem>
-                <ToggleGroupItem
-                  type="button"
-                  variant="outline"
-                  value="Variator"
-                >
-                  ვარიატორი
-                </ToggleGroupItem>
-              </ToggleGroup>
-            </FormControl>
-          </FormItem>
-        )}
+        label="ტრანსმისია"
+        errors={errors}
+        options={[
+          { value: "Manual", label: "მექანიკა" },
+          { value: "Automatic", label: "ავტომატიკა" },
+          { value: "Tiptronic", label: "ტიპტრონიკი" },
+          { value: "Variator", label: "ვარიატორი" },
+        ]}
       />
-      <Controller
+      <ToggleGroupField
         control={control}
         name="fuel"
-        render={({ field }) => (
-          <FormItem className="">
-            <FormLabel>
-              <SmallFormHeading label="საწვავის ტიპი" />
-            </FormLabel>
-            <FormMessage>{errors.fuel?.message as React.ReactNode}</FormMessage>
-            <FormControl>
-              <ToggleGroup
-                value={field.value}
-                onValueChange={(value) => field.onChange(value)}
-                className="flex gap-x-4 gap-y-2 flex-wrap pt-2"
-                type="single"
-              >
-                <ToggleGroupItem
-                  type="button"
-                  variant="outline"
-                  value="Gasoline"
-                >
-                  ბენზინი
-                </ToggleGroupItem>
-                <ToggleGroupItem type="button" variant="outline" value="Diesel">
-                  დიზელი
-                </ToggleGroupItem>
-                <ToggleGroupItem
-                  type="button"
-                  variant="outline"
-                  value="Electric"
-                >
-                  ელექტრო
-                </ToggleGroupItem>
-                <ToggleGroupItem type="button" variant="outline" value="Hybrid">
-                  ჰიბრიდი
-                </ToggleGroupItem>
-                <ToggleGroupItem
-                  type="button"
-                  variant="outline"
-                  value="RechargeableHybrid"
-                >
-                  დატენვადი ჰიბრიდი
-                </ToggleGroupItem>
-                <ToggleGroupItem
-                  type="button"
-                  variant="outline"
-                  value="LiquidGas"
-                >
-                  თხევადი გაზი
-                </ToggleGroupItem>
-                <ToggleGroupItem
-                  type="button"
-                  variant="outline"
-                  value="NaturalGas"
-                >
-                  ბუნებრივი გაზი
-                </ToggleGroupItem>
-                <ToggleGroupItem
-                  type="button"
-                  variant="outline"
-                  value="Hydrogen"
-                >
-                  წყალბადი
-                </ToggleGroupItem>
-              </ToggleGroup>
-            </FormControl>
-          </FormItem>
-        )}
+        label="საწვავის ტიპი"
+        errors={errors}
+        options={[
+          { value: "Gasoline", label: "ბენზინი" },
+          { value: "Diesel", label: "დიზელი" },
+          { value: "Electric", label: "ელექტრო" },
+          { value: "Hybrid", label: "ჰიბრიდი" },
+          { value: "RechargeableHybrid", label: "დატენვადი ჰიბრიდი" },
+          { value: "LiquidGas", label: "თხევადი გაზი" },
+          { value: "NaturalGas", label: "ბუნებრივი გაზი" },
+          { value: "Hydrogen", label: "წყალბადი" },
+        ]}
       />
     </div>
   );
---
+++ app/(protected)/add/_components/toggle-group-field.tsx
@@ -0,0 +1,60 @@
+import {
+  FormControl,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Control, Controller } from "react-hook-form";
+import * as z from "zod";
+import { SmallFormHeading } from "./small-form-heading";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+
+interface ToggleGroupFieldProps<Schema extends z.ZodType<any, any>> {
+  control: Control<z.infer<Schema>>;
+  name: keyof z.infer<Schema>;
+  label: string;
+  errors: any;
+  options: { value: string; label: string }[];
+}
+
+export const ToggleGroupField = <Schema extends z.ZodType<any, any>>({
+  control,
+  name,
+  label,
+  errors,
+  options,
+}: ToggleGroupFieldProps<Schema>) => {
+  return (
+    <Controller
+      control={control}
+      name={name as any}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>
+            <SmallFormHeading label={label} />
+          </FormLabel>
+          <FormMessage>{errors[name]?.message as React.ReactNode}</FormMessage>
+          <FormControl>
+            <ToggleGroup
+              value={field.value}
+              onValueChange={(value) => field.onChange(value)}
+              className="flex gap-x-4 gap-y-2 flex-wrap pt-2"
+              type="single"
+            >
+              {options.map(({ value, label }) => (
+                <ToggleGroupItem
+                  key={value}
+                  type="button"
+                  variant="outline"
+                  value={value}
+                >
+                  {label}
+                </ToggleGroupItem>
+              ))}
+            </ToggleGroup>
+          </FormControl>
+        </FormItem>
+      )}
+    />
+  );
+};
